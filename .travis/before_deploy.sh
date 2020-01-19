286046e2df4cf6ec6f16a020f6f1eec6af1d82c0
Dana Van Aken <dvanaken@cs.cmu.edu>
2020-01-19 08:09:46 -0500

    Save the latest git commit ID inside the Docker containers to /app/.git_commit

--- .travis/before_deploy.sh
+++ .travis/before_deploy.sh
@@ -6,7 +6,8 @@ cd tmp
 git clone "https://${GIT_TOKEN}@github.com/${GIT_ORG}/${GIT_REPO}"
 cd $GIT_REPO
 cp $ROOT/docker/install.sh $WEB/requirements.txt .
-docker-compose -f docker-compose.build.yml build
+git_commit=`git log --format="%H" -n 1`
+docker-compose -f docker-compose.build.yml build --build-arg GIT_COMMIT="$git_commit"
 
 docker tag ottertune-base "${DOCKER_REPO}:base"
 docker tag ottertune-web "${DOCKER_REPO}:web"
